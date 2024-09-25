ade0947703516b7c0b2975a63ed0e91de7772df7
alakshana <etiquettestartshere@gmail.com>
2024-09-24 21:36:54 -0400

    first commit

---
+++ scripts/featureorigin.mjs
@@ -0,0 +1,37 @@
+Hooks.once("init", FeatureOrigin.init);
+
+class FeatureOrigin {
+
+  static init() {
+    Hooks.on("renderItemSheet5e", FeatureOrigin._advancementOrigin);
+  }
+
+  static _advancementOrigin(app, [html]) {
+    const actor = app.actor;
+    if (!actor) return;
+    if (app.object.type !== "feat" || app.object?.getFlag("dnd5e", "advancementOrigin")?.includes(".")) return;
+    const current = app.object?.getFlag("dnd5e", "advancementOrigin");
+    const choices = actor.items.reduce((acc, i) => {
+      if (!i.system.advancement) return acc;
+      acc.push({
+        value: i.id,
+        label: app.object.parent.items.get(i.id).name,
+        group: game.i18n.localize(`TYPES.Item.${i.type}`)
+      });
+      return acc;
+    }, []);
+    const origins = HandlebarsHelpers.selectOptions(choices, { hash: { selected: current, sort: true } });
+    const origin = `
+      <div class="form-group">
+        <label>Feature Origin</label>
+        <select name="flags.dnd5e.advancementOrigin">
+          <option></option>
+          ${origins}
+        </select>
+      </div>
+    `
+    const type = html.querySelector('.form-group:has(select[name="system.type.subtype"])') ??
+      html.querySelector('.form-group:has(select[name="system.type.value"])');
+    type.insertAdjacentHTML("afterend", origin);
+  }
+}
